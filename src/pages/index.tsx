a4e6ea38dac2f7532986c0342b310489d537ce95
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-05-27 19:33:45 -0300

    chore: add support to tailwind

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      <p className="text-pink-500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione quae
+        debitis distinctio facilis eum! Expedita repellat magni, error sequi
+        earum molestiae ab rerum praesentium consequatur fugit quae quos, saepe
+        ducimus?
+      </p>
+    </>
+  );
+}
